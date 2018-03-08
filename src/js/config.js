4af5ae54c80b285f52656c7a1e18a5c8e04d92c9
xiaoxiaoxiaoxie <1070837384@qq.com>
2018-03-08 20:47:26 +0800

    修改demo文件

--- src/js/config.js
+++ src/js/config.js
@@ -3,7 +3,7 @@ require.config({
 	paths : {
 		jquery : "lib/jquery/jquery-1.12.4.min",
 		cookie : "lib/jquery_plugins/jquery.cookie",
-		zoom : "lib/jquery_plugins/jquery.elevateZoom-3.0.8.min",
+		zoom : "lib/jquery_plugins/jquery.elevateZoom-3.0.8.min",//放大镜的插件
 		carousel : "lib/jquery_plugins/jquery.xmcarousel",
 		template : "lib/artTemplate/template",
 		load : "js/loadHeaderFooter"
